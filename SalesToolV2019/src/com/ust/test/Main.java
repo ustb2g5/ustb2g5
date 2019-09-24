2d74ab6c7dda2fc0a3472a63fd226ab8d5c9ddd8
user71 <user71@user71-System-Product-Name>
2019-09-24 15:51:52 +0530

    3rd

--- SalesToolV2019/src/com/ust/test/Main.java
+++ SalesToolV2019/src/com/ust/test/Main.java
@@ -15,7 +15,7 @@ public class Main {
 	private static void displayGreeting() {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome");
-		System.out.println("Test 1");
+		System.out.println("Test 2");
 	}
 	
 }
